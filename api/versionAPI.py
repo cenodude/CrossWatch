a9fef810f5ce041bfd605b6db9498c1096fd69f2
Pazzie van den Berg <cenodude@outlook.com>
2026-02-01 12:58:11 +0100

    Change: update to 0.8.2

--- api/versionAPI.py
+++ api/versionAPI.py
@@ -19,7 +19,7 @@ __all__ = ["router"]
 
 router = APIRouter(prefix="/api", tags=["version"])
 
-CURRENT_VERSION = os.getenv("APP_VERSION", "v0.8.1")
+CURRENT_VERSION = os.getenv("APP_VERSION", "v0.8.2")
 REPO = os.getenv("GITHUB_REPO", "cenodude/CrossWatch")
 
 
