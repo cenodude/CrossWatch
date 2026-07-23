c109de2fb6908cc29766917ecca3b27bbe6acfa4
Pazzie van den Berg <cenodude@outlook.com>
2026-07-23 21:59:56 +0200

    Change: update version to v0.10.4

--- api/versionAPI.py
+++ api/versionAPI.py
@@ -19,7 +19,7 @@ __all__ = ["router"]
 
 router = APIRouter(prefix="/api", tags=["version"])
 
-CURRENT_VERSION = os.getenv("APP_VERSION", "v0.10.3")
+CURRENT_VERSION = os.getenv("APP_VERSION", "v0.10.4")
 REPO = os.getenv("GITHUB_REPO", "cenodude/CrossWatch")
 
 
